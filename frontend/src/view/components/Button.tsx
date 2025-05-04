0e33162d56faf4e3e74fe7a9aecc82fa1211166e
BrunaDuarte-3321 <brunaduarte3000@hotmail.coom>
2025-05-03 21:42:05 -0300

    feat:  implements validatio form signin and signup

--- frontend/src/view/components/Button.tsx
+++ frontend/src/view/components/Button.tsx
@@ -1,16 +1,16 @@
 import { ComponentProps } from "react";
+import { cn } from "../../app/utils/cn";
 
 interface IButton extends ComponentProps<"button"> {}
 
-export function Button({ disabled, children, ...props }: IButton) {
+export function Button({ className, ...props }: IButton) {
   return (
     <button
       {...props}
-      disabled={disabled}
-      className="bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100
-      px-6 h-12 rounded-2xl font-medium text-white disabled:text-gray-400 disabled:cursor-not-allowed transition-all"
-    >
-      {children}
-    </button>
+      className={cn(
+        "bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 px-6 h-12 rounded-2xl font-medium text-white disabled:text-gray-400 disabled:cursor-not-allowed transition-all",
+        className
+      )}
+    />
   );
 }
