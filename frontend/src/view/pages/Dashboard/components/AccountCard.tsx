5af42e8e03d860fb67a493d4e23b296ca67f2fe9
BrunaDuarte-3321 <brunaduarte3000@hotmail.coom>
2025-05-11 18:47:40 -0300

    feat: create account card and slider

---
+++ frontend/src/view/pages/Dashboard/components/AccountCard.tsx
@@ -0,0 +1,30 @@
+import { formatCurrency } from "../../../../app/utils/formatCurrency";
+import { BankAccountTypeIcon } from "../../../components/icons/BankAccountTypeIcon";
+
+interface IAccountCardProps {
+  color: string;
+  name: string;
+  balance: number;
+  type: "INVESTMENT" | "CASH" | "CHECKING";
+}
+export function AccountCard({ color, balance, type, name }: IAccountCardProps) {
+  return (
+    <div
+      style={{ borderColor: color }}
+      className="p-4 bg-white border rounded-2xl h-[200px] flex flex-col justify-between border-b-4 border-teal-950"
+    >
+      <div>
+        <BankAccountTypeIcon type={type} />
+        <span className="text-gray-800 font-medium tracking-[-0.5px] mt-4 block">
+          {name}
+        </span>
+      </div>
+      <div>
+        <span className="text-gray-800 font-medium  tracking-[-0.5px]  block">
+          {formatCurrency(balance)}
+        </span>
+        <small className="text-gray-600 font-sm">Saldo Atual</small>
+      </div>
+    </div>
+  );
+}
--- frontend/src/view/pages/Dashboard/components/Accounts.tsx
+++ frontend/src/view/pages/Dashboard/components/Accounts.tsx
@@ -1,7 +1,55 @@
+import { EyeIcon } from "../../../components/icons/EyeIcon";
+import { AccountCard } from "./AccountCard";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { AccountSliderNavigation } from "./AccountSliderNavigation";
+
 export function Accounts() {
   return (
-    <div className="bg-teal-900 rounded-2xl w-full h-full px-4 py-8 md:p-10">
-      <h1>Accounts</h1>
+    <div className="flex  flex-col bg-teal-900 rounded-2xl w-full h-full px-4 py-8 md:p-10">
+      <div className="">
+        <span className="tracking-[-0.5px] text-white block">Saldo total</span>
+        <div className="flex items-center gap-2">
+          <strong className="text-2xl tracking-[-1px] text-white">
+            R$ 100,00
+          </strong>
+
+          <button className="w-8 h-8 flex items-center justify-center">
+            <EyeIcon open />
+          </button>
+        </div>
+      </div>
+      <div className="flex-col flex-1 flex justify-end">
+        <div>
+          <Swiper spaceBetween={16} slidesPerView={2.1}>
+            <div
+              className="flex items-center justify-between mb-4"
+              slot="container-start"
+            >
+              <strong className="text-white tracking-[-1px] text-lg">
+                Minhas Contas
+              </strong>
+              <AccountSliderNavigation />
+            </div>
+            <div>
+              <SwiperSlide>
+                <AccountCard
+                  color="#7950F2"
+                  balance={1000.23}
+                  name="Nubank"
+                  type="CASH"
+                />
+              </SwiperSlide>
+              <SwiperSlide>
+                <AccountCard balance={50000} name="XP" type="INVESTMENT" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <AccountCard balance={50000} name="XP" type="INVESTMENT" />
+              </SwiperSlide>
+            </div>
+          </Swiper>
+        </div>
+      </div>
     </div>
   );
 }
